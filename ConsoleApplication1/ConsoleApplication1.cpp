fc27d78b039cc6fff7e37c22fb9792cce5813a40
User <User@WIN-GSN3E73J7BL>
2023-08-21 14:18:36 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int grammv;
+    int poz;
+    int vsto;
+    int gramm;
+    float g{};
+    setlocale(0, "rus");
+    cout << "Пожалуйста введите сколько всего грамм" <<endl;
+    cin >> grammv;
+    cout << "Пожалуйста введите сколько всего позиций" << endl;
+    cin >> poz;
+    cout << "Пожалуйста соберите сто штук одной позиции и запишите время в секундах" << endl;
+    cin >> vsto;
+    cout << "Пожалуйста взвести каждую позицию и запишите ее вес в граммах" << endl;
+    for (int i = 0; i < poz; i++) {
+        cout << "Позиция номер " <<i<<":";
+        cin >> gramm;
+        cout << endl;
+        g = g + gramm;
+    }
+    g = g / poz;
+    cout <<"Средний вес всех позиций: "<< g;
+}
